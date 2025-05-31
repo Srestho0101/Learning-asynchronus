63cde4e5715051dc45291a23188b1ff8982f1957
Srestho <srestho1298@gmail.com>
2025-05-31 07:49:59 +0000

    Add files via upload

--- learningFetch.js
+++ learningFetch.js
@@ -1,4 +1,4 @@
-fetch("forAsync.json")
-  .then(value => value.json())
-  .then(user => console.log(user.name))
+fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  .then(user => console.log(user.results[0].name.title))
   .catch(error => console.log(error))
